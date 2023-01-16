4e6a2b3c5d51adcd0996f1111ea0ec5cbeecf3df
Stanislav2705 <stasrogal25@gmail.com>
2023-01-16 16:19:34 +0200

    async components

---
+++ contacts.js
@@ -0,0 +1,55 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+require("colors");
+
+const contactsPath = path.resolve(__dirname, "./db/contacts.json");
+
+async function readFile() {
+  const contactsRaw = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(contactsRaw);
+  return contacts;
+}
+
+async function writeFile(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+async function listContacts() {
+  return await readFile();
+}
+
+async function getContactById(contactId) {
+  const contacts = await readFile();
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  if (!contact) {
+    throw new Error(`Contact with id:${contactId} not found`.bgRed);
+  }
+
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await readFile();
+  const updateContact = contacts.filter((contact) => contact.id !== contactId);
+  await writeFile(updateContact);
+}
+
+async function addContact(name, email, phone) {
+  const id = nanoid();
+  const contact = { id, name, email, phone };
+
+  const contacts = await readFile();
+  contacts.push(contact);
+
+  await writeFile(contacts);
+  return contact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
